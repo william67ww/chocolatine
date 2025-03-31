71169931aa50f74ceb9d76bc7f468ee287539ed2
William Wagner <williamwagner@epitech.eu>
2025-03-31 16:16:10 +0200

    correction

--- units_tests/test.c
+++ units_tests/test.c
@@ -9,10 +9,13 @@
 #include <criterion/redirect.h>
 #include "../include/my.h"
 
-test(test1, error_return_value)
+void test(int test1, int error_return_value)
 {
+    (void)test1;
+    (void)error_return_value;
+
     int argc = 0;
     char **argv = NULL;
 
-    cr_assert(error(argc, argv) == -1);
+    cr_assert(error(argc, argv) == -1, "Expected error() to return -1");
 }
